ba7593aaf373e3326a7fb93d0bb3790597639fc7
untruebench <sylviawong@vip.qq.com>
2026-07-11 23:58:28 +0800

    set site language to en

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -8,7 +8,7 @@ export default defineAstroPaperConfig({
     author: "yesut",
     profile: "https://t.me/raenut",
     ogImage: "2.JPG",
-    lang: "zh-CN",
+    lang: "en"
     timezone: "Asia/Shanghai",
     dir: "ltr",
   },
